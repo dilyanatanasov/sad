f0306455f588603529e130275ae74c2066eabf7f
Dilyan <dilyan.atanasov@scavasoft.com>
2020-08-27 21:53:43 +0300

    basic readme file update and started design

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Student assignment distribution system
+# Student Assignment Distribution System (SAD)
+
+![Distribution](https://cdn.iconscout.com/icon/free/png-512/distribution-29-1146710.png)
+
+Tool that can be used to assign topics for school assignments.
+
+The basic idea:
+- students are added to a database via interface
+- topics are added via interface
+- via the same interface a shuffle operation is started
+- students are distributed in groups of 3 and one of 4 for when it's an odd number
+
+__This way the groups will be made on a random matter.__
--- views/html/studentDistribution.php
+++ views/html/studentDistribution.php
@@ -5,19 +5,67 @@ require "shared/helper.php";
 
 $student = new StudentController();
 $result = $student->getAllStudents();
+?>
 
-echo "<html>
+<html>
 <head>
     <title></title>
     <link rel='stylesheet' type='text/css' href='../style/studentDistribution.css'>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome -->
+    <script src="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
 </head>
 <body>
     <div class='container'>
-        <h1>Student Distribution</h1>";
-foreach ($result as $singleStudent) {
-    echo "<p>Student ${singleStudent['id']} ${singleStudent['first_name']} ${singleStudent['last_name']}</p>";
-}
-echo "</div>
+        <h1>Student Assignment Distribution System</h1>
+        <div class="form-group">
+            <label for="studentsList">Students List</label>
+
+            <textarea class="form-control" id="studentsList" rows="3"></textarea>
+        </div>
+        <button class="btn btn-primary">Submit</button>
+        <table class="table mt-4">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>";
 
